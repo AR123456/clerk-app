7b39522319519a26e6dc44593f92dcfc5a5ff11e
AR123456 <coloradoz99@gmail.com>
2023-07-30 08:37:24 -0600

    css position home

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <main className="container mx-auto">
+            <div className="flex items-start justify-center min-h-screen">
+              <div className="mt-20">{children}</div>
+            </div>
+          </main>
+        </body>
       </html>
     </ClerkProvider>
   );
