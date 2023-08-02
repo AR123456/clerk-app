f087f9d0bf99d11796e8ee9b9b64aa91bc707119
AR123456 <coloradoz99@gmail.com>
2023-08-02 06:43:35 -0600

    middleware in root

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const DashboardPage = () => {
   return (
-    <div>
+    <>
       <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
       <p className="mb-5">Welcome to the Dashboard</p>
-    </div>
+    </>
   );
 };
 
