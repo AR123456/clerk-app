023a1927047377f535c20aa908572103dc85e887
AR123456 <coloradoz99@gmail.com>
2023-08-04 06:38:58 -0600

    HOME PAGE

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Link from "next/link";
+// using clerks content to show different header if loggedin
+import { auth } from "@clerk/nextjs";
+
 const Header = () => {
+  const { userId } = auth();
+  // this log is coming from clerk, from server so appears in terminal not browser
+
   return (
     <>
       <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
@@ -11,13 +17,24 @@ const Header = () => {
             </div>
           </Link>
         </div>
-        <div className="text-white">
-          <Link href="sign-in" className="text-gray-300 hover:text-white mr-4">
-            Sign In
-          </Link>
-          <Link href="sign-up" className="text-gray-300 hover:text-white mr-4">
-            Sign Up
-          </Link>
+        <div className="text-white flex items-center">
+          {!userId && (
+            <>
+              {" "}
+              <Link
+                href="sign-in"
+                className="text-gray-300 hover:text-white mr-4"
+              >
+                Sign In
+              </Link>
+              <Link
+                href="sign-up"
+                className="text-gray-300 hover:text-white mr-4"
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
         </div>
       </nav>
     </>
