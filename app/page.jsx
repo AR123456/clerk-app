6ba954fed62ae4d3b99801571a9ad0869ca990a0
AR123456 <coloradoz99@gmail.com>
2023-08-09 06:29:34 -0600

    clerk app home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-2xl font-bold mb-5">Welcome</h1>
+      <p className="mb-5">Checking out Clerk.js</p>
     </>
   );
 }
