72341bdd38e96e0f73c652a2572e64328a8eb55e
AR123456 <coloradoz99@gmail.com>
2023-08-10 05:46:49 -0600

    profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// using clerks out of box userprofile component
+import { UserProfile } from "@clerk/nextjs";
+const ProfilePage = () => {
+  return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
