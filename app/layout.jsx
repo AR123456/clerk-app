8c5ecbe3b21d36fb25bb9dbea80cb34fe79f1cf0
AR123456 <coloradoz99@gmail.com>
2023-08-11 01:55:05 -0600

    themes import to layouts

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "./components/Header";
+import { dark } from "@clerk/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
