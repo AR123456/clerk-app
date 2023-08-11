10cbfeffc336677cd8e574b7a1c19ca058902d3c
AR123456 <coloradoz99@gmail.com>
2023-08-11 01:56:59 -0600

    DYI registration page

---
+++ app/register/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//creating a custom register page - add this to middleware.js public routes
+
+const RegisterPage = () => {
+  return <div>RegisterPage</div>;
+};
+
+export default RegisterPage;
