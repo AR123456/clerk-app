ad77dce4002c6803ee946da66e22e4eacbd7f378
AR123456 <coloradoz99@gmail.com>
2023-08-12 06:01:26 -0600

    add dark theme as apperance object

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className={inter.className}>
           <Header></Header>
