4e4991743115b3b29930736e83a074ad8627da63
AR123456 <coloradoz99@gmail.com>
2023-08-13 07:18:44 -0600

    light theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "./components/Header";
-import { dark } from "@clerk/themes";
+import { light } from "@clerk/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
+    <ClerkProvider appearance={{ baseTheme: light }}>
       <html lang="en">
         <body className={inter.className}>
           <Header></Header>
