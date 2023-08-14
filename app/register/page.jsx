fd8cc22b9c620bc9d10f356ffed7f0bba7d00b7c
AR123456 <coloradoz99@gmail.com>
2023-08-14 05:55:13 -0600

    regestration page imports

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,5 +1,12 @@
-import React from "react";
 //creating a custom register page - add this to middleware.js public routes
+//client side useState ect so use client
+("use client");
+// react use state
+import { useState } from "react";
+// clerks signup
+import { useSignUp } from "@clerk/nextjs";
+// next js router
+import { useRouter } from "next/navigation";
 
 const RegisterPage = () => {
   return <div>RegisterPage</div>;
