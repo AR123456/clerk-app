d75ea858d561b485f4669c8985fa705c02d8085b
AR123456 <coloradoz99@gmail.com>
2023-08-14 05:56:16 -0600

    reg functions shell and styling

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,6 +1,6 @@
 //creating a custom register page - add this to middleware.js public routes
 //client side useState ect so use client
-("use client");
+"use client";
 // react use state
 import { useState } from "react";
 // clerks signup
@@ -10,15 +10,31 @@ import { useRouter } from "next/navigation";
 
 const RegisterPage = () => {
   //component state
+  // destructure
   const { isLoaded, signUp, setActive } = useSignUp();
+  // form fields
   const [email, setEmail] = useState("");
+  //   to use first and last name enable them on clerk dashboard
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [pendingVerification, setPendingVerification] = useState(false);
   const [code, setCode] = useState("");
   const router = useRouter();
-  return <div>RegisterPage</div>;
+
+  // Form submit
+  const handleSubmit = async (e) => {};
+  //Verify User Email Code
+  const onPressVerify = async (e) => {};
+
+  return (
+    <>
+      <div className="border p-5 rounded" style={{ width: "500px" }}>
+        {" "}
+        <h1 className="text-2xl mb-4"> Register</h1>
+      </div>
+    </>
+  );
 };
 
 export default RegisterPage;
